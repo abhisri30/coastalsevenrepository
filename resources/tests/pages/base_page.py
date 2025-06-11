a0da92170eadc62685c73c681b65e08a0b3b44f7
Jyoswanjana_CR <swanjanaravuri@gmail.com>
2025-06-11 15:12:52 +0530

    Delete resources directory

--- resources/tests/pages/base_page.py
+++
@@ -1,8 +0,0 @@
-from playwright.async_api import Page
-
-class BasePage:
-    def __init__(self, page: Page):
-        self.page = page
-
-    async def goto(self, path: str) -> None:
-        await self.page.goto(path)
